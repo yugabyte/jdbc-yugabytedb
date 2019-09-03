2f2fe4a4fe3a5298689006f815a054329537c0df
Mihnea Iancu <m-iancu@users.noreply.github.com>
2019-09-02 23:52:25 -0700

    Import pgjdbc code as of the following commit: commit fcbbc3e6408cc1bcf459b740c683f3db40a5050c Author: Matteo Melli <teoincontatto@users.noreply.github.com> Date:   Wed Jul 24 16:15:53 2019 +0200
        Updated scram to version 2.0 (#1532)

---
+++ pgjdbc/src/main/java/org/postgresql/replication/fluent/AbstractCreateSlotBuilder.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2016, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.replication.fluent;
+
+import org.postgresql.core.BaseConnection;
+import org.postgresql.core.ServerVersion;
+import org.postgresql.util.GT;
+
+import java.sql.SQLFeatureNotSupportedException;
+
+public abstract class AbstractCreateSlotBuilder<T extends ChainedCommonCreateSlotBuilder<T>>
+    implements ChainedCommonCreateSlotBuilder<T> {
+
+  protected String slotName;
+  protected boolean temporaryOption = false;
+  protected BaseConnection connection;
+
+  protected AbstractCreateSlotBuilder(BaseConnection connection) {
+    this.connection = connection;
+  }
+
+  protected abstract T self();
+
+  @Override
+  public T withSlotName(String slotName) {
+    this.slotName = slotName;
+    return self();
+  }
+
+  @Override
+  public T withTemporaryOption() throws SQLFeatureNotSupportedException {
+
+    if (!connection.haveMinimumServerVersion(ServerVersion.v10)) {
+      throw new SQLFeatureNotSupportedException(
+          GT.tr("Server does not support temporary replication slots")
+      );
+    }
+
+    this.temporaryOption = true;
+    return self();
+  }
+}
---
+++ pgjdbc/src/main/java/org/postgresql/util/PSQLException.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2003, PostgreSQL Global Development Group
+ * See the LICENSE file in the project root for more information.
+ */
+
+package org.postgresql.util;
+
+import java.sql.SQLException;
+
+public class PSQLException extends SQLException {
+
+  private ServerErrorMessage serverError;
+
+  public PSQLException(String msg, PSQLState state, Throwable cause) {
+    super(msg, state == null ? null : state.getState(), cause);
+  }
+
+  public PSQLException(String msg, PSQLState state) {
+    super(msg, state == null ? null : state.getState());
+  }
+
+  public PSQLException(ServerErrorMessage serverError) {
+    super(serverError.toString(), serverError.getSQLState());
+    this.serverError = serverError;
+  }
+
+  public ServerErrorMessage getServerErrorMessage() {
+    return serverError;
+  }
+}
